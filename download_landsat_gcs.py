a806e1b88d566112b4dc2f0ef0b4091d9facf348
Cao Zhigang <zhigang_niglas@163.com>
2019-08-28 13:30:28 -0700

    batch processing and add the s2msi downloading

--- download_landsat_gcs.py
+++ download_landsat_gcs.py
@@ -4,36 +4,75 @@
 # Download the data files of Landsat T1 from google cloud storage
 # Before you used the script to download, two things need to be prepared
 # 1. install the gstuil toolbox as referenced:
-#    https://cloud.google.com/storage/docs/gsutil_install?hl=zh-cn
 # 2. Order a list of data in earthexplorer.usgs.gov
 #    then, save their scenced ID from the Traking Bulkorders
 #    input template:
+#    https://github.com/zgcao/Landsat/blob/master/template_input_gcs
 
 from __future__ import print_function
-
 import subprocess
+from datetime import datetime
 
 
-def downd_gcs(scene_name, outdir):
-    # gsutil cp -r 199/024/LC08_L1TP_199024_20180903_20180912_01_T1/ ./
-    fileparts = scene_name.split('_')
-    mission = fileparts[0]
-    url = 'gs://gcp-public-data-landsat/' + mission + '/01/'
-    path, row = fileparts[2][0:3], fileparts[2][3:]
+def down_lc8oli_gcs(scene_list, outdir):
+    #
+    f = open(outdir + '/download_log_'+ datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'w')
 
-    download_url = url + path + '/' + row + '/' + scene_name + '/'
-    cmd = '/Users/zhigang/Scripts/google-cloud-sdk/bin/gsutil'
-    cmd = cmd + ' -m cp -r ' + download_url + ' ' + outdir
-    print(cmd)
+    for scene_name in open(filename, 'r').readlines():
+        fileparts = scene_name.strip().split('_')
+        mission = fileparts[0]
+        url = 'gs://gcp-public-data-landsat/' + mission + '/01/'
+        path, row = fileparts[2][0:3], fileparts[2][3:]
 
-    status = subprocess.call(cmd, shell=True)
-    if status != 0:
-        print(scene_name + ' Failed!')
-    return
+        download_url = url + path + '/' + row + '/' + scene_name.strip() + '/'
+        # cmd = '/Users/zhigang/Scripts/google-cloud-sdk/bin/gsutil'
+        cmd = 'gsutil -m cp -r ' + download_url + ' ' + outdir
+        print(cmd)
+
+        status = subprocess.call(cmd, shell=True)
+        if status != 0:
+            print(scene_name + ' Failed!')
+            f.write(scene_name + ' -- failed.\n')
+        else:
+            f.write(scene_name + ' -- successful!\n')
+
+    f.close()
+
+def down_s2msi_gcs(xml_file, outdir):
+    import xml.etree.cElementTree as ET
+
+    f = open(out_dir + '/download_log_'+ datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'w')
+
+    tree = ET.ElementTree(file=xml_file)
+    url = 'gs://gcp-public-data-sentinel-2/tiles/'    
+    for item in tree.iter(tag='{urn:ietf:params:xml:ns:metalink}file'):
+        file_name = item.attrib['name']
+        #print(file_name)
+        foos = file_name.split('_')
+        UTM_ZONE,LATITUDE_BAND,GRID_SQUARE = foos[5][1:3],foos[5][3],foos[5][4:]
+        #print(UTM_ZONE,LATITUDE_BAND,GRID_SQUARE,sep=',')
+        GRANULE_ID = file_name.split('.')[0] + '.SAFE'
+        download_url = url + UTM_ZONE + '/' + LATITUDE_BAND + '/' + GRID_SQUARE + '/' + GRANULE_ID
+
+        cmd = 'gsutil -m cp -r ' + download_url + ' ' + outdir
+        print(cmd)
+
+        status = subprocess.call(cmd, shell=True)
+        if status != 0:
+            print(GRANULE_ID + ' Failed!')
+            f.write(GRANULE_ID + ' Failed!\n')
+        else:
+            f.write(cmd + ' -- successful!\n')
+
+    f.close()
+
 
 
 """Main"""
-filename = '/Volumes/mac_zhigang/Satellite_Data/Wuliangsuhai/LE07'
-outdir = '/Volumes/mac_zhigang/Satellite_Data/Wuliangsuhai/'
-for scene_name in open(filename, 'r').readlines():
-    downd_gcs(scene_name.strip(), outdir)
+filename = '/Users/zhigang/Downloads/bulk_1033158.txt'
+outdir = '/Users/zhigang/Downloads'
+down_lc8oli_gcs(filename, outdir)
+
+xml_file = '/Users/zhigang/Downloads/products-2.meta4'
+out_dir = '/Volumes/Data2/MSI_Taihu_2015_2018'
+down_s2msi_gcs(xml_file, out_dir)
